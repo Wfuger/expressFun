eb19694406598c5543c3e4a098462b0fcd89f2d9
Wfuger <wfuger@gmail.com>
2016-04-03 20:34:38 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict'
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const _ = require('lodash');
+let users = [];
+
+fs.readFile('users.json', {encoding: 'utf8'}, function(err, data){
+  if (err) throw error
+
+  JSON.parse(data).forEach((user) => {
+    user.name.full = _.startCase(user.name.first + ' ' + user.name.last)
+    users.push(user)
+  });
+});
+
+app.get('/', (req, res) => {
+  let buffer = ''
+  users.forEach((user) => {
+    buffer += '<a href="/' + user.username + '">' + user.name.full + '</a><br>'
+  })
+res.send(buffer);
+})
+
+
+const server = app.listen(5000, () => {
+  console.log("Server running at http://localhost:" + server.address().port);
+});
